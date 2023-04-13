3c8afb25efdc43f29a39ba60a17bbdd7726edb31
nobeeeeel <nobelchua@yahoo.com.sg>
2023-04-14 01:59:37 +0800

    Added the rollover
    Added the rollover

--- project/ProjectManagementSystem.java
+++ project/ProjectManagementSystem.java
@@ -1,17 +1,44 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class ProjectManagementSystem {
-    private List<Project> projects;
-
-    public ProjectManagementSystem() {
+public ProjectManagementSystem() {
         projects = new ArrayList<Project>();
-    }
+        lastProjectId = 0;
+        loadProjectsFromExcel();
+        }
 
-    // Add a project to the system
+// Load projects from rollover project file
+    private void loadProjectsFromExcel() {
+        File rolloverProjectFile = new File("rollover project.xlsx");
+        try {
+        Scanner scanner = new Scanner(rolloverProjectFile);
+        // Skip the first line which is the header
+        if (scanner.hasNextLine()) {
+        scanner.nextLine();
+        }
+        while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        String[] parts = line.split(",");
+        String supervisor = parts[0];
+        String title = parts[1];
+        Project project = new Project(++lastProjectId, supervisor, title);
+        addProject(project);
+        }
+        scanner.close();
+        } catch (IOException e) {
+        System.out.println("Failed to load rollover project file: " + e.getMessage());
+        }
+        }
+
+// Add a project to the system
     public void addProject(Project project) {
+        project.setId(++lastProjectId);
         projects.add(project);
-    }
+        }
+
 
     // Get all projects in the system
     public List<Project> getProjects() {
